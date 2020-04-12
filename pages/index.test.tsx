dce0740d36507dfdf05a6bb068ff384160ab2682
Roberta <>
2020-04-12 18:04:49 +0100

    Created a new Next project with TypeScript, Jest and React Testing Library.

---
+++ pages/index.test.tsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import HomePage from './index'
+
+test('renders a message', () => {
+  const { container, getByText } = render(<HomePage />)
+  
+  expect(getByText('Welcome to Next.js!')).toBeInTheDocument()
+
+  expect(container.firstChild).toMatchSnapshot()
+})
