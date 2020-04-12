408f9cebdc9028f6a66eb5279673ffa3a0d95252
Roberta <>
2020-04-12 19:04:28 +0100

    Setup ESLint and Prettier

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,13 +1,13 @@
-import '@testing-library/jest-dom/extend-expect'
-import React from 'react'
-import { render } from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect"
+import React from "react"
+import { render } from "@testing-library/react"
 
-import HomePage from './index'
+import HomePage from "./index"
 
-test('renders a message', () => {
-  const { container, getByText } = render(<HomePage />)
-  
-  expect(getByText('Welcome to Next.js!')).toBeInTheDocument()
+test("renders a message", () => {
+    const { container, getByText } = render(<HomePage />)
 
-  expect(container.firstChild).toMatchSnapshot()
+    expect(getByText("Welcome to Next.js!")).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
 })
