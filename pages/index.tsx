55d40124393de4ba4eb4ae39fe454cb65fbf4362
Roberta <3443568+rogood@users.noreply.github.com>
2021-10-03 15:57:49 +0100

    Installed Cypress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,9 +14,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <nav>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </nav>
 
         <p className={styles.description}>
           Get started by editing{' '}
